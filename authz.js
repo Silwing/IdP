a27856e73cf92bb118c9f1cd5d140ddd36edc25e
Silwing <silwing@gmail.com>
2014-11-14 15:18:33 +0100

    Initial commit

---
+++ authz.js
@@ -0,0 +1,162 @@
+var http = require("http"),
+	url = require("url"),
+	qs = require("querystring"),
+	clientSeverName = "localhost",
+	clientSeverPort = "8888",
+	clientSever = "http://" + clientSeverName + ":" + clientSeverPort + "/",
+	users = [{username: "user1",password: "1234"},{username: "user2", password: "4321"}],
+	clients = [{cid: "cid123456", code: "ac123", token: "at42"}];
+
+
+(function start() {
+
+	
+	function onRequest(request, response) {
+		var urlObj = url.parse(request.url, true);
+		
+		switch (urlObj.pathname) {
+			case "/login":
+				login(request,response,urlObj);
+				break;
+			case "/authenticate":
+				authenticate(request,response,urlObj);			
+				break;
+			case "/access":
+				access(request,response,urlObj);			
+				break;
+			case "/verify":
+				verify(request,response,urlObj);				
+				break;
+			default:
+				console.log(request.url);
+				error(response,"We could not find anything matching your request... :'(");
+		}
+	}
+	
+	/*
+	*	Handle login request (3/4)
+	*	Get login request from client through the browser and send request to user/browser
+	*/
+	function login(request,response,urlObj) {
+		var clientid = urlObj.query.clientId;
+		var responsetype = urlObj.query.responseType;
+		var returnurl = urlObj.query.returnurl;
+			
+		if(responsetype != "code"){
+			error(response,"The response type must be 'code'");
+		}
+			
+		if(!checkClientId(response, clientid)) {
+			return;
+		}
+			
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		//todo: returner login side til browser
+		response.write("login");
+		console.log(clientid);
+		console.log(responsetype);
+			
+		response.end();
+	}
+	
+	/*
+	*	Handle authentication of user (7/9)
+	*	Get info from user and send to client through the browser
+	*/
+	function authenticate(request,response,urlObj) {	
+		var username = urlObj.query.username;
+		var password = urlObj.query.password;
+		var clientid = urlObj.query.clientId;
+		var returnurl = urlObj.query.returnUrl;
+		var valid = false;
+
+		for(i = 0; i < users.length; i++){
+			if(username == users[i].username && password == users[i].password){
+				valid = true;
+				break;
+			}
+		}
+					
+		if(valid){
+			for(i = 0; i < clients.length; i++){
+				if(clientid == clients[i].cid){
+					response.writeHead(303, {"Location": returnUrl + "?" + qs.stringify({authzCode: clients[i].code})});
+					response.end();
+					return;
+				}
+			}
+			error(response, "Invalid client ID");
+		} else {
+			error(response,"Wrong username or password");			
+		}
+	}
+	
+	/*
+	*	Handle access request from client (10/12)
+	*	Communicate directly with client
+	*/
+	function access(request,response,urlObj) {
+		var clientid = urlObj.query.clientId;
+		var acode = urlObj.query.authzCode;
+		
+		for(i = 0; i < clients.length; i++){
+			if(clientid == clients[i].cid && acode == clients[i].code){
+				response.writeHead(200, {"Content-Type": "application/json"});
+				response.write(JSON.stringify({accessToken: clients[i].token, tokenType: "bearer"}));
+				response.end();
+				return;
+			}
+		}
+		error(response,JSON.stringify({error: "ClientId or authorization code not valid"}));		
+	}
+	
+	/*
+	*	Handle verification of access (17) 
+	* 	Communicate with the resource server
+	*/
+	function verify(request,response,urlObj) {
+		var token = urlObj.query.accessToken;
+		var valid = false;
+		
+		if(accesstokens.length > 0){
+			for(i = 0; i < accesstokens.length; i++){
+				if(token == accesstokens[i]){
+					valid = true;
+					break;
+				}
+			}
+		}
+		
+		if(valid){
+			// todo: Returnerer valid eller invalid
+			response.writeHead(200, {"Content-Type": "text/html"});
+			response.write("verify");
+			response.write(" valid accesstoken");
+			response.end();
+		} else{
+			// error
+			error(response,"Not a valid access token");
+		}
+		
+	}
+	
+	
+	function checkClientId(response, clientid) {
+		for(var i = 0; i < clients.length; i++) {
+			if(clients[i].cid == clientid)
+				return true;
+		}
+		error(response, "Client with id " + clientid + " is not a registered client");
+		return false;
+	}
+	
+	function error(response, msg) {
+		response.writeHead(400, {"Content-Type": "text/plain"});
+		response.write(msg);
+		response.end();
+		return;
+	}
+	
+	http.createServer(onRequest).listen(8889);
+
+})();
---
+++ client.js
@@ -0,0 +1,177 @@
+var http = require("http"),
+	url = require("url"),
+	qs = require("querystring"),
+	nock = require("nock"), // TODO: remove in final version
+	authzServer = { hostname: "localhost", 
+	                port: 8889,
+	                paths: {
+	                	redirect: "/login",
+	                	access: "/access"
+	                }},
+	resServer = { hostname: "localhost",
+	              port: 8890,
+	              paths: {
+	              	info: "/info"
+	              }},
+	selfServer = { hostname: "localhost", 
+	               port: 8888,
+	               paths: {
+	               	authorize: "/authorize"
+	             }},
+	clientID = "",
+	responseType = "code",
+	accessToken = undefined, tokenType = undefined;
+
+nock("http://" + resServer.hostname + ":" + resServer.port)
+.filteringPath(function(path) {
+	return resServer.paths.info;
+})
+.get(resServer.paths.info)
+.reply(200, {name: "Testnavn"});
+
+(function start() {
+
+	function sendHeader(response) {
+		response.writeHead(200, {"Content-Type": "text/html"});
+		response.write("<!DOCTYPE html>");
+		response.write("<html>");
+		response.write("<head>");
+		response.write("<title>Most Fantastic OAuth App</title>");
+		response.write("</head>");
+		response.write("<body>");
+	}
+
+	function sendFooter(response) {
+		response.write("</body>");
+		response.write("</html>");
+		response.end();
+	}
+
+	function getServerString(serverObj) {
+		return "http://" + serverObj.hostname + ":" + serverObj.port;
+	}
+
+	function getRequest(options, callback) {
+		return http.get(options, callback);
+	}
+
+	function mainPage(request, response, urlObj) {
+		sendHeader(response);
+		response.write("<h1>Welcome</h1>");
+		response.write("<p>To start using this wonderful application click on the login button below.</p>");
+		response.write("<div class='login-wrapper'><a href='/init'>Login</a>");
+		sendFooter(response);
+	}
+
+	function init(request, response, urlObj) {
+	    response.writeHead(303, {"Location": 
+	    	                           getServerString(authzServer) + authzServer.paths.redirect + "?" + 
+	    	                           qs.stringify({
+	    	                           	clientId: clientID, 
+	    	                           	responseType: responseType,
+	    	                           	returnUrl: getServerString(selfServer) + selfServer.paths.authorize
+	    	                           })});
+	    response.end();		
+	}
+
+	function authorize(request, response, urlObj) {
+		var authzCode = urlObj.query.authzCode,
+		    authOptions = { hostname: authzServer.hostname,
+		    	            port: authzServer.port,
+		    	            path: authzServer.paths.access + "?" + qs.stringify({authzCode: authzCode, clientId: clientID})
+        	        };
+
+        if(!authzCode) {
+        	response.writeHead(400, {"Content-Type": "text/plain"});
+        	response.write("Your request was invalid. Please use only valid requests.");
+        	response.end();
+        	return;
+        }
+
+        var authReq = getRequest(authOptions, function(authResp) {
+        	if(authResp.statusCode == 200) {
+        		authResp.setEncoding("utf8");
+        		var jsonData = "";
+        		authResp.on("data", function(chunk) {
+        			jsonData += chunk;
+        		})
+        		.on("end", function() {
+        			var dataObj = JSON.parse(jsonData);
+        			accessToken = dataObj.accessToken;
+        			tokenType = dataObj.tokenType;
+        		})
+        		.on("end", function() {
+        			var resOptions = { hostname: resServer.hostname,
+        				               port: resServer.port,
+        				               path: resServer.paths.info + "?" + qs.stringify({accessToken: accessToken, tokenType: tokenType})
+        			},
+        			    resReq = getRequest(resOptions, function(resResp) {
+        			    	if(resResp.statusCode == 200) {
+        			    		var resData = "";
+        			    		resResp.on("data", function(chunk) {
+        			    			resData += chunk;
+        			    		})
+        			    		.on("end", function() {
+        			    			var resObj = JSON.parse(resData);
+        			    			sendHeader(response);
+        			    			response.write("<p>Hej " + resObj.name + "</p>");
+        			    			response.write("<p>Tillykke! Du er nu logget ind.</p>");
+        			    			sendFooter(response);
+        			    		});
+        			    	}
+        			    });
+        		});
+        	} else if(authResp.statusCode == 400) {
+        		authResp.setEncoding("utf8");
+        		var jsonData = "";
+        		authResp.on("data", function(chunk) {
+        			jsonData += chunk;
+        		})
+        		.on("end", function() {
+        			var dataObj = JSON.parse(jsonData);
+        			sendHeader(response);
+        			response.write("<p>Vi kunne desværre ikke logge dig ind :(</p>");
+        			sendFooter(response);
+        		})
+        	}
+        });
+	}
+
+	function onRequest(request, response) {
+		var urlObj = url.parse(request.url, true);
+
+		switch(urlObj.pathname) {
+
+			case "/":
+			    mainPage(request, response, urlObj);
+			break;
+
+			case "/init":
+			    init(request, response, urlObj);
+			break;
+
+			case "/authorize":
+			    authorize(request, response, urlObj);
+			break;
+
+			default:
+			    response.writeHead(404, {"Content-Type": "text/plain"});
+			    response.write("We could not find anything matching your request... :'(");
+				response.end();
+			break;
+		}
+	}
+
+	if(process.argv.length < 3) {
+		console.log("Usage: node client.js clientID [port] [hostname]");
+		return;
+	}
+	if(process.argv.size > 3)
+		selfServer.port = process.argv[3];
+	if(process.argv.size > 4)
+		selfServer.hostname = process.argv[4];
+
+	clientID = process.argv[2];
+	http.createServer(onRequest).listen(selfServer.port);
+
+})();
